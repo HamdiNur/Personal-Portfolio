9886f8d852f596c0fae012a8d7c7b8c23128a2c0
HamdiNur <hamdoonur26@gmail.com>
2026-06-17 07:33:10 +0300

    update info section with accurate personal details

--- src/component/Info.jsx
+++ src/component/Info.jsx
@@ -14,7 +14,7 @@ const Info = () => {
       <i class="bx bx-briefcase-alt about__icon"></i>
 
          <h3 className="about__title">Completed</h3>
-         <span className="about__subtitle">15 + project</span>
+         <span className="about__subtitle">25 + project</span>
       </div>
 
       <div className="about__box">
