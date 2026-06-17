8e8ff84f0be607e457c53af954b9251abb34f31b
HamdiNur <hamdoonur26@gmail.com>
2026-06-17 07:33:58 +0300

    add slick carousel css imports to main entry

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import Home from './Pages/Home'
 import './global.css'
 
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <Home/>
